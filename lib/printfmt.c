c3e1f7a8fc7b851ae25b5371edef92efcd426e8c
enana <l694895849@gmail.com>
2022-12-06 09:10:12 +0800

    lab1 completed

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -206,10 +206,9 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			putch('X', putdat);
-			putch('X', putdat);
-			putch('X', putdat);
-			break;
+			num = getuint(&ap, lflag);
+			base = 8;
+			goto number;
 
 		// pointer
 		case 'p':
@@ -232,6 +231,7 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		case '%':
 			putch(ch, putdat);
 			break;
+
 
 		// unrecognized escape sequence - just print it literally
 		default:
